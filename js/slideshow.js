4debe1f264d1d6d64c2fe1043fc9b6c3b900db26
Pallman <d67h412@msu.montana.edu>
2019-04-10 11:41:57 -0600

    Initial Upload

---
+++ js/slideshow.js
@@ -0,0 +1,14 @@
+var currentSlide = 0;
+slideshow();
+
+function slideshow() {
+  var i;
+  var slides = document.getElementsByClassName("slideshow-slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  currentSlide++;
+  if (currentSlide > slides.length) {currentSlide = 1} 
+  slides[currentSlide-1].style.display = "block"; 
+  setTimeout(slideshow, 5000); 
+}
