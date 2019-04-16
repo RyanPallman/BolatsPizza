1566a4cca5931997b0e97a407b05598197c2a7bf
cookieman768 <cookieman768@gmail.com>
2019-04-16 10:10:25 -0600

    Minor Tweaks
    * Discovered that the navbar shifting from the home to the other pages
    is because the scroll bar disappears which affects the width of the
    page.
    * Added images to the index replacing the placeholder text.
    * Made the buttons on the slideshow look better.
    * Changed the navbar to use a class so when unordered lists are used
    elsewhere they don't look like the navbar.
    * Formatted the HTML, CSS, and JavaScript to make it easier to read.

--- js/slideshow.js
+++ js/slideshow.js
@@ -4,13 +4,14 @@ function slideshow() {
   var i;
   var slides = document.getElementsByClassName("slideshow-slide");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
+    slides[i].style.display = "none";
   }
   currentSlide++;
-  if (currentSlide > slides.length)
-    {currentSlide = 1};
-  slides[currentSlide-1].style.display = "block"; 
-  setTimeout(slideshow, 5000); 
+  if (currentSlide > slides.length) {
+    currentSlide = 1
+  };
+  slides[currentSlide - 1].style.display = "block";
+  setTimeout(slideshow, 5000);
 }
 
 slideshow();
